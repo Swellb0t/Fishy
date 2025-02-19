a300b641d66ebc843c18cd7cdc41ac1ed8208956
Swellb0t <87591802+Swellb0t@users.noreply.github.com>
2025-02-19 13:57:53 -0500

    venv change
    venv change

--- bin/activate.csh
+++ bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/frink/pythonprojects/fishy"
+setenv VIRTUAL_ENV "/home/frink/pythonprojects/fishs2/Fishy"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -17,8 +17,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(fishy) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(fishy) "
+    set prompt = "(Fishy) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(Fishy) "
 endif
 
 alias pydoc python -m pydoc
--- bin/activate.fish
+++ bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/frink/pythonprojects/fishy"
+set -gx VIRTUAL_ENV "/home/frink/pythonprojects/fishs2/Fishy"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -56,7 +56,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(fishy) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(Fishy) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -65,5 +65,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(fishy) "
+    set -gx VIRTUAL_ENV_PROMPT "(Fishy) "
 end
